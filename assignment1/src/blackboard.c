3c2c42edb0e693ed7d082b0d162ad50b2473f7ae
Mamo1031 <mamo.ota1031@gmail.com>
2025-02-09 18:28:04 +0100

    fix: make backboard holds multiple obstacles and targets

--- assignment1/src/blackboard.c
+++ assignment1/src/blackboard.c
@@ -19,10 +19,23 @@
 #define OBSTACLES_PIPE_NAME PIPE_DIR "pipe_to_obstacles"
 #define TARGETS_PIPE_NAME PIPE_DIR "pipe_to_targets"
 
-// Blackboard structure holds drone, target, and obstacle data.
+// Define FIFO capacities for targets and obstacles
+#define MAX_TARGETS 50
+#define MAX_OBSTACLES 50
+
+// Blackboard structure holds drone data and FIFO queues for targets and obstacles.
 typedef struct {
     int drone_x;
     int drone_y;
+    // Each target: [0]: x, [1]: y
+    int targets[MAX_TARGETS][2];
+    int target_head;  // index of the oldest target
+    int target_tail;  // index where next target will be enqueued
+    // Each obstacle: [0]: x, [1]: y
+    int obstacles[MAX_OBSTACLES][2];
+    int obstacle_head;
+    int obstacle_tail;
+
     int target_x;
     int target_y;
     int obstacle_x;
@@ -60,6 +73,27 @@ void read_parameters(const char *filename, int *max_clients, int *buffer_size, d
     fclose(file);
 }
 
+// Enqueue a target into the FIFO queue.
+void enqueue_target(Blackboard *bb, int tx, int ty) {
+    bb->targets[bb->target_tail][0] = tx;
+    bb->targets[bb->target_tail][1] = ty;
+    bb->target_tail = (bb->target_tail + 1) % MAX_TARGETS;
+    // If the queue is full, advance head to overwrite oldest element.
+    if (bb->target_tail == bb->target_head) {
+        bb->target_head = (bb->target_head + 1) % MAX_TARGETS;
+    }
+}
+
+// Enqueue an obstacle into the FIFO queue.
+void enqueue_obstacle(Blackboard *bb, int ox, int oy) {
+    bb->obstacles[bb->obstacle_tail][0] = ox;
+    bb->obstacles[bb->obstacle_tail][1] = oy;
+    bb->obstacle_tail = (bb->obstacle_tail + 1) % MAX_OBSTACLES;
+    if (bb->obstacle_tail == bb->obstacle_head) {
+        bb->obstacle_head = (bb->obstacle_head + 1) % MAX_OBSTACLES;
+    }
+}
+
 // handle_client processes incoming messages and updates the Blackboard accordingly.
 void handle_client(int fd, char *buffer, Blackboard *bb) {
     ssize_t bytes_read = read(fd, buffer, 1023);
@@ -69,13 +103,18 @@ void handle_client(int fd, char *buffer, Blackboard *bb) {
     if (strncmp(buffer, "OBSTACLE,", 9) == 0) {
         int ox, oy;
         sscanf(buffer, "OBSTACLE,%d,%d", &ox, &oy);
-        bb->obstacle_x = ox;
-        bb->obstacle_y = oy;
+        enqueue_obstacle(bb, ox, oy);
     } else if (strncmp(buffer, "TARGET,", 7) == 0) {
         int tx, ty;
         sscanf(buffer, "TARGET,%d,%d", &tx, &ty);
-        bb->target_x = tx;
-        bb->target_y = ty;
+        enqueue_target(bb, tx, ty);
+    } else if (strncmp(buffer, "DRONE:", 6) == 0) {
+        int x, y;
+        // Expected format: "DRONE: (x,y)"
+        if (sscanf(buffer, "DRONE: (%d,%d)", &x, &y) == 2) {
+            bb->drone_x = x;
+            bb->drone_y = y;
+        }
     } else if (strcmp(buffer, "UPDATE_DRONE") == 0) {
         bb->drone_x += 1;
         bb->drone_y += 1;
@@ -83,14 +122,30 @@ void handle_client(int fd, char *buffer, Blackboard *bb) {
 }
 
 // write_to_pipe sends the current state of the Blackboard via the BLACKBOARD_PIPE_NAME.
+// It outputs the drone position, followed by the FIFO contents for targets and obstacles.
 void write_to_pipe(int pipe_fd, Blackboard *bb) {
     char buffer[1024];
+    char target_str[512] = "";
+    char obstacle_str[512] = "";
+    int i = bb->target_head;
+    while (i != bb->target_tail) {
+        char temp[32];
+        snprintf(temp, sizeof(temp), "(%d,%d)", bb->targets[i][0], bb->targets[i][1]);
+        strcat(target_str, temp);
+        i = (i + 1) % MAX_TARGETS;
+        if (i != bb->target_tail) strcat(target_str, ",");
+    }
+    i = bb->obstacle_head;
+    while (i != bb->obstacle_tail) {
+        char temp[32];
+        snprintf(temp, sizeof(temp), "(%d,%d)", bb->obstacles[i][0], bb->obstacles[i][1]);
+        strcat(obstacle_str, temp);
+        i = (i + 1) % MAX_OBSTACLES;
+        if (i != bb->obstacle_tail) strcat(obstacle_str, ",");
+    }
     snprintf(buffer, sizeof(buffer),
-             "Drone: (%d, %d), Target: (%d, %d), Obstacle: (%d, %d)",
-             bb->drone_x, bb->drone_y,
-             bb->target_x, bb->target_y,
-             bb->obstacle_x, bb->obstacle_y
-    );
+             "Drone: (%d, %d), Targets: [%s], Obstacles: [%s]",
+             bb->drone_x, bb->drone_y, target_str, obstacle_str);
 
     ssize_t bytes_written = write(pipe_fd, buffer, strlen(buffer) + 1);
     if (bytes_written == -1) {
@@ -101,7 +156,6 @@ void write_to_pipe(int pipe_fd, Blackboard *bb) {
     }
 }
 
-
 int main(int argc, char *argv[]) {
     signal(SIGPIPE, SIG_IGN);
 
@@ -110,7 +164,7 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    int wd_fd = open(WATCHDOG_PIPE_NAME, O_WRONLY);
+    int wd_fd = open(WATCHDOG_PIPE_NAME, O_WRONLY | O_NONBLOCK);
     if (wd_fd == -1) {
         perror("Failed to open watchdog pipe in blackboard");
     }
@@ -118,6 +172,19 @@ int main(int argc, char *argv[]) {
     int max_clients, buffer_size;
     double drone_mass, viscous_coeff;
     read_parameters(argv[1], &max_clients, &buffer_size, &drone_mass, &viscous_coeff);
+
+    // Initialize Blackboard.
+    Blackboard bb;
+    bb.drone_x = 0;
+    bb.drone_y = 0;
+    bb.target_x = 0;    // (Not used in FIFO implementation)
+    bb.target_y = 0;
+    bb.obstacle_x = 0;  // (Not used in FIFO implementation)
+    bb.obstacle_y = 0;
+    bb.target_head = 0;
+    bb.target_tail = 0;
+    bb.obstacle_head = 0;
+    bb.obstacle_tail = 0;
 
     // List of named pipes to read from.
     const char *pipe_names[] = {
@@ -157,7 +224,6 @@ int main(int argc, char *argv[]) {
     }
 
     fd_set read_fds_set;
-    Blackboard bb = {0, 0, 0, 0, 0, 0};
     char buffer[buffer_size];
 
     while (1) {
@@ -174,7 +240,7 @@ int main(int argc, char *argv[]) {
             }
         }
 
-        // Wait indefinitely for data to be available.
+        // Wait indefinitely for data.
         int activity = select(max_fd + 1, &read_fds_set, NULL, NULL, NULL);
         if (activity < 0) {
             perror("Select error");
@@ -193,14 +259,17 @@ int main(int argc, char *argv[]) {
         write_to_pipe(write_pipe_fd, &bb);
 
         // Notify Watchdog.
-        if (wd_fd != -1) {
+        if (wd_fd != -1 && fcntl(wd_fd, F_GETFL) != -1) {
             char msg[64];
             snprintf(msg, sizeof(msg), "HEARTBEAT,blackboard,%d", getpid());
             ssize_t ret = write(wd_fd, msg, strlen(msg) + 1);
             if (ret < 0) {
                 perror("Failed to write to watchdog pipe");
-            } else if (ret < (ssize_t)(strlen(msg) + 1)) {
-                fprintf(stderr, "Partial write: %ld/%lu bytes\n", (long)ret, (long)strlen(msg) + 1);
+                close(wd_fd);
+                wd_fd = open(WATCHDOG_PIPE_NAME, O_WRONLY | O_NONBLOCK);
+                if (wd_fd == -1) {
+                    perror("Failed to reopen watchdog pipe");
+                }
             }
         }
     }
@@ -212,7 +281,6 @@ int main(int argc, char *argv[]) {
         }
     }
     close(write_pipe_fd);
-    // Optionally close dummy writer fds.
     if (dummy_targets_fd != -1) close(dummy_targets_fd);
     if (dummy_obstacles_fd != -1) close(dummy_obstacles_fd);
     if (wd_fd != -1) close(wd_fd);
